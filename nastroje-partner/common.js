d41884ff14c395414e78daf4d7b9744569e6b772
ANODIUS\fordost <tamas.fordos@anodius.com>
2022-12-29 13:45:48 +0100

    debug 9

--- nastroje-partner/common.js
+++ nastroje-partner/common.js
@@ -273,7 +273,7 @@
     //https://eu.coresuite.com/cloud-org-level-service/api/v1/levels/allocations?unifiedPersonId=B78E5239-34A1-475E-AEF3-99329C68203A&includeSubLevels=false
     // unifiedPersonId=${unifiedPersonIdWithDashes}&includeSubLevels=false`
     let hhh = await common.getHeadersForOrgLevel();
-    console.log('headers before fetch XXX: ', hhh)
+    console.log('headers before fetch 545454545: ', hhh)
 
     const responseOrgLevel = await fetch(
       `https://eu.coresuite.com/cloud-org-level-service/api/v1/levels/allocations?unifiedPersonId=${unifiedPersonIdWithDashes}&includeSubLevels=false`,
