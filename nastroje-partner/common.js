919dc4b08977fe2a61cb3a845b0f68f72c86fb84
ANODIUS\fordost <tamas.fordos@anodius.com>
2023-01-02 16:24:29 +0100

    credentials 2

--- nastroje-partner/common.js
+++ nastroje-partner/common.js
@@ -273,7 +273,7 @@
     //https://eu.coresuite.com/cloud-org-level-service/api/v1/levels/allocations?unifiedPersonId=B78E5239-34A1-475E-AEF3-99329C68203A&includeSubLevels=false
     // unifiedPersonId=${unifiedPersonIdWithDashes}&includeSubLevels=false`
     let hhh = await common.getHeadersForOrgLevel();
-    console.log('headers before fetch origin-when-cross-origin: ', hhh)
+    console.log('headers  using credentials: ', hhh)
  // `https://eu.coresuite.com/cloud-org-level-service/api/v1/levels/allocations?unifiedPersonId=${unifiedPersonIdWithDashes}&includeSubLevels=false`,
     const responseOrgLevel = await fetch(
       `https://eu.coresuite.com/cloud-org-level-service/api/v1/levels/allocations?unifiedPersonId=${unifiedPersonIdWithDashes}&includeSubLevels=false`,
