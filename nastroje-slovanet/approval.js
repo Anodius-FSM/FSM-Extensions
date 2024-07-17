73ecb9afa8df9af3141a4cda07d138753100151f
ANODIUS\fordost <tomi29a@protonmail.com>
2024-07-17 12:09:46 +0200

    zmena te.startDateTime na te.udf.z_f_te_approvedate, pridana podmienka sc.udf.z_f_sc_fixstav IN ('OK', 'OK_SYSTEM'  v funkcii fetchTable()

--- nastroje-slovanet/approval.js
+++ nastroje-slovanet/approval.js
@@ -142,8 +142,9 @@ const approval = (() => {
             AND priceList.udf.z_f_co_km IS NOT NULL
             WHERE 
               (p.businessPartner = '${businessPartnerId}'
-              AND (te.startDateTime >= '${since}' AND te.startDateTime < '${until}')
+              AND (te.udf.z_f_te_approvedate >= '${since}' AND te.udf.z_f_te_approvedate <= '${until}')
               AND ap.decisionStatus = 'APPROVED'
+              AND sc.udf.z_f_sc_fixstav IN ('OK', 'OK_SYSTEM')
               AND (m IS NOT NULL AND te IS NOT NULL)) 
 			  ${filtersQuery ? "AND  " + filtersQuery : ""}
             ORDER BY te.startDateTime DESC
