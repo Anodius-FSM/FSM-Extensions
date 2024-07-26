9a37160a7ba65d39cbcb715cb0cf332203a5e650
ANODIUS\fordost <tomi29a@protonmail.com>
2024-07-26 11:23:01 +0200

    test bagu s datummi v schvalovacej appke

--- nastroje-slovanet/approval.js
+++ nastroje-slovanet/approval.js
@@ -81,7 +81,11 @@ const approval = (() => {
     }
 
     const since = moment(monthYear, 'MM/YYYY').startOf('month').toISOString().replace(/\.000Z$/, 'Z').split('T')[0];
+    console.log("🚀 ~ fetchTable ~ since:", since);
     const until = moment(monthYear, 'MM/YYYY').add(1, 'month').startOf('month').toISOString().replace(/\.000Z$/, 'Z').split('T')[0];
+    console.log("🚀 ~ fetchTable ~ until:", until);
+
+    
     
     const filtersQuery = Object.entries(filters)
       .map(([colName, value]) => {
