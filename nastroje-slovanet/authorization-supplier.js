63704fe85178003021ce3508e3895e67790ed8a4
FilipZarnovicky <108924165+FilipZarnovicky@users.noreply.github.com>
2025-12-30 14:58:25 +0100

    Update authorization-supplier.js

--- nastroje-slovanet/authorization-supplier.js
+++ nastroje-slovanet/authorization-supplier.js
@@ -14,7 +14,7 @@
     let page = 1;
     while (true) {
       const response = await fetch(
-        'https://eu.coresuite.com/api/query/v1?' + new URLSearchParams({
+        'https://eu.fsm.cloud.sap/api/query/v1?' + new URLSearchParams({
           ...await common.getSearchParams(),
           dtos: 'UdoValue.9;UdoMeta.9;BusinessPartner.23',
           pageSize: 1000,
@@ -142,7 +142,7 @@
 
     if (entriesToUpdate.length) {
       const responseForUpdate = await fetch(
-        'https://eu.coresuite.com/api/data/v4/UdoValue/bulk?' + new URLSearchParams({
+        'https://eu.fsm.cloud.sap/api/data/v4/UdoValue/bulk?' + new URLSearchParams({
           ...await common.getSearchParams(),
           dtos: 'UdoValue.9',
           forceUpdate: true,
@@ -175,7 +175,7 @@
 
     if (entriesToCreate.length) {
       const responseForCreate = await fetch(
-        'https://eu.coresuite.com/api/data/v4/UdoValue/bulk?' + new URLSearchParams({
+        'https://eu.fsm.cloud.sap/api/data/v4/UdoValue/bulk?' + new URLSearchParams({
           ...await common.getSearchParams(),
           dtos: 'UdoValue.9',
           forceUpdate: true,
@@ -208,7 +208,7 @@
 
     if (entriesToRemove.length) {
       const responseForDelete = await fetch(
-        'https://eu.coresuite.com/api/data/v4/UdoValue/bulk?' + new URLSearchParams({
+        'https://eu.fsm.cloud.sap/api/data/v4/UdoValue/bulk?' + new URLSearchParams({
           ...await common.getSearchParams(),
           forceDelete: true,
         }),
